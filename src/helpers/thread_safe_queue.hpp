3e9fc7b0cbbd6dd05224f5ad404d5874f52fb607
Joshua Thompson <joshuareecet@gmail.com>
2026-06-28 00:38:26 +0100

    implemented max queue size, push methods now return false on full queue, true on successful push. added fps data to class.

--- src/helpers/thread_safe_queue.hpp
+++ src/helpers/thread_safe_queue.hpp
@@ -6,7 +6,7 @@
 #include <memory>
 #include <exception>
 
-namespace helpers{
+namespace mtq{
     
     struct EmptyQueue : std::exception{
         const char* what() const noexcept override {
@@ -17,18 +17,26 @@ namespace helpers{
     template <typename T>   
     class ThreadSafeQueue{
     private:
+    
     std::deque<T> queue_{};
-    mutable std::mutex mutex_{};
-
+    std::size_t max_queue_size_ {};
+    double fps_ {60};
+    
+    mutable std::mutex mq_{};
+    mutable std::mutex mdata_{};
+    
+    
     public:
         // Constructors
-        ThreadSafeQueue() = default;
+        ThreadSafeQueue()
+        : max_queue_size_ {10} {}
+        
 
-        ThreadSafeQueue(std::initializer_list<T> init_list)
-            : queue_{init_list} {}
+        ThreadSafeQueue(std::initializer_list<T> init_list, std::size_t max_queue_size)
+            : queue_{init_list} , max_queue_size_{max_queue_size} {}
         
         ThreadSafeQueue(const ThreadSafeQueue& other){
-            std::lock_guard<std::mutex> lock(other.mutex_);
+            std::lock_guard<std::mutex> lock(other.mq_);
             queue_ = other.queue_;
         }
         
@@ -36,38 +44,45 @@ namespace helpers{
         ThreadSafeQueue(ThreadSafeQueue&&) = delete;
         ThreadSafeQueue& operator=(ThreadSafeQueue&&) = delete;
 
-        
-        // copy snapshot of entire queue
+        // get snapshot of entire deque
         std::deque<T> copy_queue() const{
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             return queue_;
         }
         
         bool empty() const {
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             return queue_.empty();
         }
 
         // push, pop methods
-        void push_back(T value){
-            std::lock_guard<std::mutex> lock(mutex_);
-            queue_.push_back(std::move(value));
+        bool push_back(T& value){
+            std::lock_guard<std::mutex> lock(mq_);
+            if (queue_.size() < max_queue_size_) {
+                queue_.push_back(std::move(value));
+                return true;
+            }
+            return false;
         }
         
-        void push_front(T value){
-            std::lock_guard<std::mutex> lock(mutex_);
-            queue_.push_front(std::move(value));
+        bool push_front(T& value){
+            std::lock_guard<std::mutex> lock(mq_);
+            if (queue_.size() < max_queue_size_){
+                queue_.push_front(std::move(value));
+                return true;
+            }
+            return false;
         }
         
         void pop_back(T& value){            
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             if (queue_.empty()) throw EmptyQueue();
             value = queue_.back();
             queue_.pop_back();
         }
         
         std::shared_ptr<T> pop_back(){
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             if (queue_.empty()) throw EmptyQueue();
             std::shared_ptr<T> const res = std::make_shared<T>(queue_.back());
             queue_.pop_back();
@@ -75,23 +90,34 @@ namespace helpers{
         }
 
         void pop_front(T& value){
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             if (queue_.empty()) throw EmptyQueue();            
             value = queue_.front();
             queue_.pop_front();
         }
         std::shared_ptr<T> pop_front(){
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             if (queue_.empty()) throw EmptyQueue();
             
             std::shared_ptr<T> const res = std::make_shared<T>(queue_.front());
             queue_.pop_front();
             return res;
         }
+        void set_fps(double fps){
+            std::lock_guard<std::mutex> lock_fps{mdata_};
+            if (fps < 0){
+                throw std::invalid_argument("ERROR: invalid fps, must be at least 0");
+            }
+            fps_ = fps;
+        }
+        double get_fps(){
+            std::lock_guard<std::mutex> lock_fps{mdata_};
+            return fps_;
+        }
 
         // operator overloads
         T operator()(std::size_t i){
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             if (i >= queue_.size()) {
                 throw std::invalid_argument("ERROR: invalid index!");
             }
@@ -99,7 +125,7 @@ namespace helpers{
         }
                 
         std::size_t size() const{
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mq_);
             return queue_.size();
         }
         
